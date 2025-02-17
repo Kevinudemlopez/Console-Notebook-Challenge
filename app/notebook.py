31f7047b47fef21d646f1f5579d1b5129615bab8
Kevin Stiven Aguirre Lopez <kaguirre070@soyudemedellin.edu.co>
2025-02-17 13:06:36 -0500

    Cree clase Note

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,33 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+from datetime import datetime
+
+class Note:
+    HIGH = "HIGH"
+    MEDIUM = "MEDIUM"
+    LOW = "LOW"
+
+    def __init__(self, code: str, title: str, text: str, importance: str):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.creation_date = datetime.now()
+        self.tags = []
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return f"Date: {self.creation_date}\n{self.title}: {self.text}"
+
+
+
+
+
+
+
+
+
+
